9cfbff761979e642e6d3daa36708107b4a6bb2dd
Julian <sequeira.julian@gmail.com>
2019-04-26 21:10:38 +1000

    Create colors.py

---
+++ 102/colors.py
@@ -0,0 +1,12 @@
+VALID_COLORS = ['blue', 'yellow', 'red']
+
+
+def print_colors():
+    """Ask for color, lowercase it, check if 'quit' is entered, if so print
+       'bye' and break, next check if given color is in VALID_COLORS, if not,
+       continue, finally if that check passes, print the color"""
+    while True:
+        pass
+        
+for i in VALID_COLORS:
+    print(i)
